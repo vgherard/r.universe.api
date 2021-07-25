80975a65e667ab2db379e04ed40ddeb78c308cb2
vgherard <vgherard@sissa.it>
2021-07-25 15:03:39 +0200

    Use my R-universe hostname in skip_if_offline()

--- tests/testthat/test-runiv_descriptions.R
+++ tests/testthat/test-runiv_descriptions.R
@@ -1,5 +1,5 @@
 test_that("runiv_descriptions() returns a data.frame", {
-        skip_if_offline()
+        skip_if_offline("vgherard.r-universe.dev")
         res <- runiv_descriptions("vgherard")
         expect_s3_class(res, class(data.frame()), exact = TRUE)
 })
--- tests/testthat/test-runiv_packages.R
+++ tests/testthat/test-runiv_packages.R
@@ -1,4 +1,4 @@
 test_that("runiv_packages() returns a character as declared", {
-        skip_if_offline()
+        skip_if_offline("vgherard.r-universe.dev")
         expect_vector(runiv_packages("vgherard"), ptype = character())
 })
