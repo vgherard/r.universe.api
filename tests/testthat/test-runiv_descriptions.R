235b31181205f47d86664dc8ec3a0b7cdc2a2b22
vgherard <vgherard@sissa.it>
2021-07-25 14:56:04 +0200

    Skip tests if offline

--- tests/testthat/test-runiv_descriptions.R
+++ tests/testthat/test-runiv_descriptions.R
@@ -1,4 +1,5 @@
 test_that("runiv_descriptions() returns a data.frame", {
+        skip_if_offline()
         res <- runiv_descriptions("vgherard")
         expect_s3_class(res, class(data.frame()), exact = TRUE)
 })
--- tests/testthat/test-runiv_packages.R
+++ tests/testthat/test-runiv_packages.R
@@ -1,3 +1,4 @@
 test_that("runiv_packages() returns a character as declared", {
+        skip_if_offline()
         expect_vector(runiv_packages("vgherard"), ptype = character())
 })
