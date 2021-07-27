e3a7d60791c4390ee05ce32268151c0f0c6bdbfc
vgherard <vgherard@sissa.it>
2021-07-27 16:01:41 +0200

    Fix typos

--- tests/testthat/test-assertions.R
+++ tests/testthat/test-assertions.R
@@ -1,7 +1,7 @@
-test_that("assert_string", {
+test_that("assert_is_string", {
         expect_error(assert_is_string("a string"), NA)
 
-        class <- domain_error_class()
+        class <- "runiv_domain_error"
         expect_error(assert_is_string(1), class = class)
         expect_error(assert_is_string(c("a", "b")), class = class)
         expect_error(assert_is_string(NA_character_), class = class)
