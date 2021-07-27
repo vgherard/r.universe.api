4621e5ce7228d16a7b4b83730251ee56b69a5b64
vgherard <vgherard@sissa.it>
2021-07-27 17:09:21 +0200

    Add stats_sysdeps

---
+++ R/runiv_stats_sysdeps.R
@@ -0,0 +1,37 @@
+# runiv
+# Copyright (C) 2021  Valerio Gherardi
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#' @title External Dependencies
+#'
+#' @description Get list with external libraries used in an R-universe universe.
+#'
+#' @inheritParams runiv_packages
+#'
+#' @return a \code{data.frame}.
+#'
+#' @examples runiv_stats_sysdeps("vgherard")
+#'
+#' @author Valerio Gherardi
+#'
+#' @export
+runiv_stats_sysdeps <- function(universe)
+{
+        assert_is_string(universe)
+        response <- runiv_api_req(
+                universe, path = "stats/sysdeps", method = "GET"
+        )
+        parse_ndjson_response(response)
+}
