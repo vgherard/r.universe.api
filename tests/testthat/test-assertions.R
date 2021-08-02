29bb9566d96671aef5071c092baf9f86fb2093bf
vgherard <vgherard@sissa.it>
2021-08-02 11:13:45 +0200

    Test assert_has_internet

--- tests/testthat/test-assertions.R
+++ tests/testthat/test-assertions.R
@@ -6,3 +6,13 @@ test_that("assert_is_string", {
         expect_error(assert_is_string(c("a", "b")), class = class)
         expect_error(assert_is_string(NA_character_), class = class)
 })
+
+test_that("Test no error if connected to internet", {
+        skip_if_not(curl::has_internet())
+        expect_error(assert_has_internet(), NA)
+})
+
+test_that("Test error if no internet connection", {
+        Sys.setenv("SIMULATE_OFFLINE" = TRUE)
+        expect_error(assert_has_internet(), class = "runiv_offline_error")
+})
